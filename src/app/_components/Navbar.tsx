90f0765d5e722d211c5b5c7e6662836c4ed31cb7
Arman <romawka47@gmail.com>
2023-11-08 20:34:43 +0600

    added next ui

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react'
+import Link from 'next/link'
+
+export default function CustomNavbar() {
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link href="/">Home</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="/about">About</Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Link from 'next/link'
 import { ReactNode } from 'react'
+import { Providers } from '@/app/providers'
+import CustomNavbar from '@/app/_components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,21 +15,10 @@ export async function generateMetadata({ params }: { [key: string]: any }) {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-black text-white`}>
-        <header className="bg-gray-400 w-full">
-          <nav>
-            <ul className="flex justify-between px-5 py-1">
-              <li>
-                <Link href="/">Home</Link>
-              </li>
-              <li>
-                <Link href="about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        {children}
+    <html lang="en" className="dark">
+      <body className={`${inter.className}`}>
+        <CustomNavbar />
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
