5a80103f7cf9f2d956bc244d5339bb32b90ac25f
Arman Kenjebay <romawka47@gmail.com>
2023-11-15 12:51:18 +0600

    added bassgold page

---
+++ src/app/[locale]/bassgold/page.tsx
@@ -0,0 +1,21 @@
+import { getDictionary } from '@/get-dictionary'
+import { Locale } from '@/i18n-config'
+
+export async function generateMetadata({ params }: any) {
+  const dictionary = await getDictionary(params.locale)
+
+  return {
+    title: dictionary.direction.bassgold,
+    description: dictionary.bassgold.bassgold,
+  }
+}
+
+export default async function BassGold({
+  params: { locale },
+}: {
+  params: { locale: Locale }
+}) {
+  const dictionary = await getDictionary(locale)
+
+  return <section className="flex flex-col w-full h-full"></section>
+}
