5d727c2dcc7af13267ae7c4578f2ff5835cc5bbb
Arman <romawka47@gmail.com>
2023-11-24 20:51:50 +0600

    added news detail

--- src/app/[locale]/_components/PrevNews.tsx
+++ src/app/[locale]/_components/PrevNews.tsx
@@ -1,20 +1,21 @@
 'use client'
 
 import Link from 'next-intl/link'
-import { ReactNode } from 'react'
 
 type Props = {
-  children?: ReactNode
-  locale: 'en' | 'ru'
   dictionary: any
+  locale: 'en' | 'ru'
+  classNames?: string
 }
 
-export default function PrevNews({ locale, dictionary }: Props) {
+export default function PrevNews({ locale, dictionary, classNames }: Props) {
   return (
     <Link
       href={`/news`}
       lang={locale}
-      className={`group flex items-center w-fit gap-x-[15px] xl:gap-x-[30px] text-base sm:text-lg md:text-2xl lg:text-3xl xl:text-4xl font-[300]`}
+      className={`${
+        classNames ? classNames : ''
+      } group flex items-center w-fit gap-x-[15px] xl:gap-x-[30px] text-base sm:text-lg md:text-2xl lg:text-3xl xl:text-4xl font-[300]`}
     >
       <div
         className={`rounded-[99px] w-[32px] h-[32px] flex items-center justify-center xl:w-[56px] xl:h-[56px] text-black bg-white ease-in-out duration-200 cursor-pointer group-hover:bg-primary-gold group-hover:text-white`}
--- src/app/[locale]/news/[id]/page.tsx
+++ src/app/[locale]/news/[id]/page.tsx
@@ -1,6 +1,8 @@
+import Image from 'next/image'
 import { getNewsById } from '@/get-api'
 import { getDictionary } from '@/get-dictionary'
 import PrevNews from '@/app/[locale]/_components/PrevNews'
+import { Chip } from '@nextui-org/chip'
 
 type Props = {
   params: {
@@ -19,12 +21,60 @@ export default async function NewsDetail({ params: { locale, id } }: Props) {
   const dictionary = await getDictionary(locale)
   const data = await getData(id).then((res) => res)
 
+  let main: any
+
+  try {
+    main = require(`@/../public/images/${data?.image?.main}`)
+  } catch (e) {
+    console.error(e)
+  }
+
   return (
     <div className={`mt-20 flex flex-col w-full h-full`}>
       <div className={`flex flex-col sm:px-5 px-2 sm:mb-10 mb-5`}>
-        <PrevNews locale={locale} dictionary={dictionary} />
+        <PrevNews
+          locale={locale}
+          dictionary={dictionary}
+          classNames={'mb-5 sm:mb-10'}
+        />
 
-        <pre>{data && data?.title?.[locale]}</pre>
+        <div
+          className={`flex gap-x-12 gap-y-3 lg:flex-row flex-col-reverse mb-5 sm:mb-10`}
+        >
+          {main && (
+            <Image
+              className={`rounded-2xl`}
+              src={main}
+              width={959}
+              height={650}
+              alt={'bassholding news image'}
+            />
+          )}
+
+          <div className={`flex flex-col`}>
+            <div className={`flex flex-row lg:flex-col`}>
+              <Chip
+                variant={'bordered'}
+                className="border-primary-gold text-primary-gold mb-10"
+                classNames={{
+                  base: 'truncate',
+                  content: 'p-0 sm:p-1 truncate',
+                }}
+              >
+                {data?.chips?.map((chi) => chi[locale])[0]}
+              </Chip>
+
+              <span className={`xl:text-[40px] font-[300] mb-10`}>
+                {data.date && data?.date[locale]}
+              </span>
+
+              <span className={`xl:text-[48px] font-[300]`}>
+                {data.title && data?.title[locale]}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={``}></div>
       </div>
     </div>
   )
