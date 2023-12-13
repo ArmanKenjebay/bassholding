a8ca876f30bc5ab3412f65c4841f762d0102c2f5
Arman Kenjebay <romawka47@gmail.com>
2023-12-13 10:45:40 +0600

    fix mobile

--- src/app/[locale]/_components/HomeBackgroundImage.tsx
+++ src/app/[locale]/_components/HomeBackgroundImage.tsx
@@ -22,7 +22,7 @@ export default function HomeBackgroundImage({
   return (
     <div className={`relative overflow-hidden ${className ?? ''}`}>
       <div
-        className={`absolute z-20 px-3 pb-16 lg:pb-24 w-full h-full flex flex-col justify-end`}
+        className={`absolute z-20 px-3 pb-[52px] sm:pb-14 md:pb-16 lg:pb-24 w-full h-full flex flex-col justify-end`}
       >
         {children}
       </div>
