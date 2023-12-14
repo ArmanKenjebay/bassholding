80395d2184426a85c3b0d9bfebbd5dca23a8e69a
Arman Kenjebay <romawka47@gmail.com>
2023-12-14 16:00:00 +0600

    fix mobile

--- src/app/[locale]/(main)/news/[id]/page.tsx
+++ src/app/[locale]/(main)/news/[id]/page.tsx
@@ -6,6 +6,7 @@ import { Chip } from '@nextui-org/chip'
 import { Locale } from '@/i18n-config'
 import Link from 'next/link'
 import PageWrapper from '@/app/[locale]/_components/PageWrapper'
+import { Skeleton } from '@nextui-org/skeleton'
 
 async function getData(id: string) {
   const data = await getNewsById(id)
@@ -21,11 +22,11 @@ export async function generateMetadata({ params }: any) {
   }
 }
 
-export default async function NewsDetail({
-  params: { locale, id },
-}: {
+type Props = {
   params: { locale: Locale; id: string }
-}) {
+}
+
+export default async function NewsDetail({ params: { locale, id } }: Props) {
   const dictionary = await getDictionary(locale)
   const data = await getData(id).then((res) => res)
 
@@ -60,38 +61,50 @@ export default async function NewsDetail({
               />
             )}
 
-            <div className={`flex flex-col gap-y-3`}>
+            <div className={`flex flex-col gap-y-3 min-w-[200px]`}>
               <div
                 className={`flex flex-row lg:flex-col gap-x-5 gap-y-3 items-center lg:items-start`}
               >
-                <Chip
-                  variant={'bordered'}
-                  className="border-primary-gold text-primary-gold xl:mb-10 mb-5"
-                  classNames={{
-                    base: 'truncate xl:text-[16px]',
-                    content: 'p-0 sm:p-1 truncate',
-                  }}
-                >
-                  {data?.chips?.map((chi) => chi[locale])[0]}
-                </Chip>
+                {data?.chips?.length && data?.chips[0][locale] ?
+                  <Chip
+                    variant={'bordered'}
+                    className="border-primary-gold text-primary-gold xl:mb-10 mb-5"
+                    classNames={{
+                      base: 'truncate xl:text-[16px]',
+                      content: 'p-0 sm:p-1 truncate',
+                    }}
+                  >
+                    {data?.chips?.map((chi) => chi[locale])[0]}
+                  </Chip>
+                  : <></>}
 
-                <span className={`xl:text-[20px] font-[300] xl:mb-10 mb-5`}>
+                {data.date && data?.date[locale] ? <span className={`xl:text-[20px] font-[300] xl:mb-10 mb-5`}>
                   {data.date && data?.date[locale]}
-                </span>
+                </span> : <Skeleton className={`inline-block w-1/2 h-3 rounded-lg`}>
+                  <div className={`w-full h-full`}></div>
+                </Skeleton>}
               </div>
               <span
                 className={`xl:text-[32px] lg:text-[32px] text-lg leading-normal font-[300]`}
               >
-                {data.title && data?.title[locale]}
+                {data.title && data?.title[locale] ? <span className={`xl:text-[20px] font-[300] xl:mb-10 mb-5`}>
+                  {data?.title[locale]}
+                </span> : <Skeleton className={`inline-block w-full h-4 rounded-lg`}>
+                  <div className={`w-full h-full`}></div>
+                </Skeleton>}
               </span>
             </div>
           </div>
           <div className={`sm:mb-10`}>
-            <span
+
+            {data.content && data.content[locale] ? <span
               className={`xl:text-[20px] inline-block whitespace-pre-line leading-normal text-sm lg:text-2xl font-[200] mb-5`}
             >
               {data.content && data.content[locale]}
-            </span>
+            </span> : <Skeleton className={`inline-block w-full h-4 rounded-lg`}>
+              <div className={`w-full h-full`}></div>
+            </Skeleton>}
+
 
             {data?.docsHref?.length && (
               <div className={`flex gap-3`}>
