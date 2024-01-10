cf179ff742fed934e00860a9717db9dcd1c57a47
Arman Kenjebay <romawka47@gmail.com>
2024-01-10 14:37:27 +0600

    fix getFirstValidUrl

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_IMAGE_API_HOST,
+        hostname: '**',
         pathname: '/uploads/**',
       },
     ],
--- src/app/[locale]/_components/NewsPreviews.tsx
+++ src/app/[locale]/_components/NewsPreviews.tsx
@@ -35,6 +35,15 @@ export default async function NewsPreviews({ locale, searchParams }: Props) {
     news = await response.json()
   }
 
+  const getFirstValidUrl = (...urls: (string | undefined)[]): string => {
+    for (const url of urls) {
+      if (url) {
+        return url
+      }
+    }
+    return ''
+  }
+
   return (
     <div className={`flex flex-col gap-y-10`}>
       {news && news.data.length ? (
@@ -46,13 +55,21 @@ export default async function NewsPreviews({ locale, searchParams }: Props) {
                     key={news.id}
                     className="cursor-pointer transition duration-200 ease-in-out group hover:scale-[.98] overflow-hidden flex-1 flex flex-col"
                   >
-                    <Image
+                    <img
                       width={808}
                       height={900}
                       className="transition duration-200 ease-in-out rounded-3xl 2xl:h-[300px] xl:h-[280px] lg:h-[180px] md:h-[120px] sm:h-[100px] w-full h-[200px] object-cover md:mb-10 mb-5"
                       src={
                         process.env.NEXT_PUBLIC_IMAGE_API +
-                        news.attributes.image_preview.data.attributes.url
+                        getFirstValidUrl(
+                          news.attributes?.image_preview?.data?.attributes
+                            .url ||
+                            news.attributes?.image_main?.data?.attributes.url ||
+                            news.attributes?.image_content?.data?.attributes
+                              .url ||
+                            news.attributes?.image_sub_content?.data?.attributes
+                              .url,
+                        )
                       }
                       alt={'bassholding news image'}
                     />
