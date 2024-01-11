4ad861a032e5a7eeb9af2a86d0fab119c40e60f5
Arman Kenjebay <romawka47@gmail.com>
2024-01-11 16:05:08 +0600

    added ir start

--- src/app/[locale]/(main)/investors/page.tsx
+++ src/app/[locale]/(main)/investors/page.tsx
@@ -2,6 +2,8 @@ import NextImage from 'next/image'
 import wallImage from '@/../public/images/company_profile.webp'
 import { Locale } from '@/i18n-config'
 import { getDictionary } from '@/get-dictionary'
+import CalculateModal from '@/app/[locale]/_components/CalculateModal'
+import FinancialTabs from '@/app/[locale]/_components/FinancialTabs'
 
 type Props = {
   params: { locale: Locale }
@@ -9,9 +11,16 @@ type Props = {
 export default async function Investors({ params: { locale } }: Props) {
   const dictionary = await getDictionary(locale)
 
+  const ebidta = [
+    { label: 'EBIDTA', value: '457 млн ₸' },
+    { label: 'ROA', value: '31.4 %' },
+    { label: 'ROE', value: '58.5%' },
+    { label: 'Вклад в развитие', value: '> 2 млрд ₸' },
+  ]
+
   return (
     <div className={`flex flex-col h-full xl:mb-10 mb-5`}>
-      <div className={`w-full`}>
+      <div className={`w-full relative xl:mb-0 mb-5`}>
         <NextImage
           className={`w-full xl:h-[484px] lg:h-[280px] md:h-[180px] sm:h-[140px] h-[121px] object-cover`}
           width={1440}
@@ -19,6 +28,35 @@ export default async function Investors({ params: { locale } }: Props) {
           src={wallImage.src}
           alt="bassholding image"
         />
+        <CalculateModal classNames={{ main: 'absolute top-1/3 right-10' }} />
+      </div>
+
+      <div className={`lg:flex flex-col p-[60px] hidden sm:mb-10 mb-5`}>
+        <span className={`text-primary-gold text-[48px]`}>BASS GOLD</span>
+        <span className={`text-[24px]`}>
+          золотодобывающая компания, имеющая 25-летний опыт деятельности на
+          рынке драгоценных металлов. В активах компании имеется золоторудное
+          месторождение Ушшокы, расположенное в области Ұлытау Республики
+          Казахстан и Чинасыл - Сай в Алматинской области, на которой
+          планируется строительство фабрики по переработке флотоконцентрата.
+        </span>
+      </div>
+
+      <div
+        className={`lg:flex lg:flex-row flex-col p-[60px] justify-evenly items-center hidden sm:mb-10 mb-5`}
+      >
+        {ebidta.map((el) => (
+          <div className={`flex flex-col text-[32px]`}>
+            <span className={``}>{el.label}</span>
+            <span className={`text-primary-gold`}>{el.value}</span>
+          </div>
+        ))}
+      </div>
+
+      <div
+        className={`xl:py-[35px] xl:px-[60px] lg:px-[40px] md:px-[32px] px-[20px] flex flex-col`}
+      >
+        <FinancialTabs />
       </div>
     </div>
   )
---
+++ src/app/[locale]/_components/CalculateModal.tsx
@@ -0,0 +1,44 @@
+type Props = {
+  classNames?: {
+    main?: string
+  }
+}
+export default async function CalculateModal({ classNames }: Props) {
+  return (
+    <div
+      className={`w-[520px] py-[23px] xl:flex flex-col gap-y-3 items-center justify-between rounded-xl bg-white hidden ${
+        classNames && classNames?.main ? classNames.main : ''
+      }`}
+    >
+      <div className={`text-black`}>
+        <span className={`text-[32px] font-normal`}>Курс золота</span>
+      </div>
+
+      <div className={`flex flex-col gap-y-3 items-center text-[21px]`}>
+        <div>
+          <span className={`text-black mr-1`}>2 061,91</span>
+          <span className={`text-red-600`}>&#8595;</span>
+          <span className={`text-red-600`}>-56.79 -2.68%</span>
+        </div>
+
+        <div className={`flex gap-x-1 text-[14px]`}>
+          <select
+            className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
+          >
+            <option>Gold</option>
+          </select>
+          <select
+            className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
+          >
+            <option>USD</option>
+          </select>
+          <select
+            className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
+          >
+            <option>oz</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  )
+}
