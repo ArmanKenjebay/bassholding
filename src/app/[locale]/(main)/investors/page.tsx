c596a72fd845e6d27ed760c0cca1b58cdfcab02d
Arman Kenjebay <romawka47@gmail.com>
2024-01-11 12:29:10 +0600

    added ir start

--- src/app/[locale]/(main)/investors/page.tsx
+++ src/app/[locale]/(main)/investors/page.tsx
@@ -1,13 +1,19 @@
-import { Image } from '@nextui-org/image'
 import NextImage from 'next/image'
 import wallImage from '@/../public/images/company_profile.webp'
+import { Locale } from '@/i18n-config'
+import { getDictionary } from '@/get-dictionary'
 
-export default async function Investors() {
+type Props = {
+  params: { locale: Locale }
+}
+export default async function Investors({ params: { locale } }: Props) {
+  const dictionary = await getDictionary(locale)
+
   return (
-    <div className={`flex flex-col h-full`}>
+    <div className={`flex flex-col h-full xl:mb-10 mb-5`}>
       <div className={`w-full`}>
         <NextImage
-          className={`w-full h-[484px] object-cover`}
+          className={`w-full xl:h-[484px] lg:h-[280px] md:h-[180px] sm:h-[140px] h-[121px] object-cover`}
           width={1440}
           height={484}
           src={wallImage.src}
