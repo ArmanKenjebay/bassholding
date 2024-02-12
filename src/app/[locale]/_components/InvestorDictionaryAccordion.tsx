c4a53194acd89046adb2d394812b2d3affd243a8
Arman Kenjebay <romawka47@gmail.com>
2024-02-12 18:31:13 +0600

    add fix khan

--- src/app/[locale]/_components/InvestorDictionaryAccordion.tsx
+++ src/app/[locale]/_components/InvestorDictionaryAccordion.tsx
@@ -75,106 +75,150 @@ export default function InvestorDictionaryAccordion({
       ],
     },
     {
-      title: 'Информация для инвесторов',
+      title: dictionary.corporate_event_calendar.investorInfo.title,
       content: [
         {
-          subTitle:
-            'Когда ТОО «BASS Gold» разместил облигации на бирже KASE?  ',
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_1,
           text: [
-            'Впервые в апреле 2023 года в торговой системе KASE состоялись специализированные торги по размещению 100 000 облигаций ТОО «BASS Gold» (ранее – ТОО «Форпост»).',
+            dictionary.corporate_event_calendar.investorInfo.subTitle_1_text_1,
           ],
         },
         {
-          subTitle: 'Где я могу узнать размер уставного капитала «BASS Gold»?',
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_2,
           text: [
-            'Данная информация представлена на нашем официальном сайте bassholding.com в разделе «Инвесторам», а также на сайте Казахстанской фондовой биржи KASE.',
+            dictionary.corporate_event_calendar.investorInfo.subTitle_2_text_1,
           ],
         },
         {
-          subTitle: 'Как приобрести облигации ТОО «BASS Gold»?',
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_3,
           text: [
-            'Приобрести облигации ТОО «BASS Gold» можно на сайте Казахстанской фондовой биржи KASE (альтернативная площадка KASE, категория «коммерческие облигации»).',
+            dictionary.corporate_event_calendar.investorInfo.subTitle_3_text_1,
           ],
         },
         {
-          subTitle: 'Какой объем реализованных облигаций ТОО «BASS Gold»? ',
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_4,
           text: [
-            'По состоянию на январь 2024 года реализовано более 100 000 облигаций ТОО «BASS Gold». ',
+            dictionary.corporate_event_calendar.investorInfo.subTitle_4_text_1,
           ],
         },
         {
-          subTitle:
-            'Какой процент процент доходности облигаций ТОО «BASS Gold»? ',
-          text: ['Процент доходности облигаций ТОО «BASS Gold» – 10,75%. '],
-        },
-        {
-          subTitle: 'Кто является ответственным по работе с инвесторами? ',
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_5,
           text: [
-            'Оственное лицо по работе с инвесторами Канаев Берик',
-            '',
-            '',
-            'Тел.: +7 707 223 34 48',
+            dictionary.corporate_event_calendar.investorInfo.subTitle_5_text_1,
           ],
         },
         {
-          subTitle: 'Какие планы по развитию ТОО «BASS Gold»? ',
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_6,
           text: [
-            'В 2024 году планируется запуск золотоизвлекательной фабрики на месторождении Ушшокы. ',
-            'Результат – выход готовой продукции в сплаве Доре',
-            'Для наращивания объема добычи и переработки твердых полезных ископаемых в 2023 году приобретено месторождение «Чинасыл-сай» в Алматинской области. Данная сделка позволила увеличить запасы золота на объем порядка 7 тонн.',
-            'В 2024 году запланировано освоение месторождения с последующим строительством фабрики по переработке флотоконцентрата производственной мощностью 300 тыс. тонн руды в год с получением товарного свинцового и цинкового концентрата с содержанием золота, серебра и кадмия.',
+            dictionary.corporate_event_calendar.investorInfo.subTitle_6_text_1,
+            dictionary.corporate_event_calendar.investorInfo.subTitle_6_text_2,
+            dictionary.corporate_event_calendar.investorInfo.subTitle_6_text_3,
+          ],
+        },
+        {
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_7,
+          text: [
+            dictionary.corporate_event_calendar.investorInfo.subTitle_7_text_1,
+          ],
+        },
+        {
+          subTitle: dictionary.corporate_event_calendar.investorInfo.subTitle_8,
+          text: [
+            dictionary.corporate_event_calendar.investorInfo.subTitle_8_text_1,
+            dictionary.corporate_event_calendar.investorInfo.subTitle_8_text_2,
+            dictionary.corporate_event_calendar.investorInfo.subTitle_8_text_3,
+            dictionary.corporate_event_calendar.investorInfo.subTitle_8_text_4,
+            dictionary.corporate_event_calendar.investorInfo.subTitle_8_text_5,
           ],
         },
       ],
     },
     {
-      title: 'Финансовая информация',
+      title: dictionary.corporate_event_calendar.financialInformation.title,
       content: [
         {
-          subTitle: 'Когда заканчивается финансовый год компании?',
+          subTitle:
+            dictionary.corporate_event_calendar.financialInformation.subTitle_1,
           text: [
-            'Финансовый год ТОО «BASS Gold» совпадает с календарным и заканчивается 31 декабря.',
-          ],
-        },
-        {
-          subTitle: 'Где можно ознакомиться с отчетностью ТОО «BASS Gold»?\n',
-          text: [
-            'Квартальные отчеты опубликованы на нашем официальном сайте bassholding.com в разделе «Инвесторам». Также документы представлены на сайте Казахстанской фондовой биржи KASE.',
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_1_text_1,
           ],
         },
         {
           subTitle:
-            'Как я могу получить финансовую отчетность ТОО «BASS Gold» предыдущих годов?',
+            dictionary.corporate_event_calendar.financialInformation.subTitle_2,
           text: [
-            'Данную информацию Вы можете запросить в индивидуальном порядке. Ответственное лицо – Канаев Берик.',
-            'Тел.: +7 707 223 34 48 ',
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_2_text_1,
           ],
         },
         {
-          subTitle: 'Какие ежегодные расходы ТОО «BASS Gold»?',
+          subTitle:
+            dictionary.corporate_event_calendar.financialInformation.subTitle_3,
           text: [
-            'Согласно финансовому прогнозу, ежегодные расходы ТОО «BASS Gold» составляют около 30 млн долларов США. ',
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_3_text_1,
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_3_text_2,
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_3_text_3,
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_3_text_4,
+          ],
+        },
+        {
+          subTitle:
+            dictionary.corporate_event_calendar.financialInformation.subTitle_4,
+          text: [
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_4_text_1,
+          ],
+        },
+        {
+          subTitle:
+            dictionary.corporate_event_calendar.financialInformation.subTitle_5,
+          text: [
+            dictionary.corporate_event_calendar.financialInformation
+              .subTitle_5_text_1,
           ],
         },
       ],
     },
     {
-      title: 'Общая информация: ',
+      title: dictionary.corporate_event_calendar.generalInformation.title,
       content: [
         {
           subTitle:
-            'Какой процент доли участия ТОО «BASS Gold» в уставном капитале Группы компаний Bass Holding? ',
+            dictionary.corporate_event_calendar.generalInformation.subTitle_1,
           text: [
-            'В сентябре 2023 года Министерством промышленности и строительства Республики Казахстан согласован переход 100% доли участия в уставном капитале ТОО «BASS Gold» (ранее – ТОО «Форпост»), принадлежащей Садвакасову Ч.М. (50%) и Тасбулатову Е.Т. (50%), в пользу ТОО «BASS Holding». Отмечаем, что доли участия в уставном капитале ТОО «BASS Holding» также принадлежат Садвакасову Ч.М. и Тасбулатову Е.Т. в равных долях.',
-            'Таким образом подтверждаем приверженность лучшим стандартам корпоративного управления с целью обеспечения прозрачности принимаемых решений и открытости перед инвесторами и партнерами группы компаний BASS Holding.',
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_1_text_1,
           ],
         },
         {
-          subTitle: 'Ключевые бизнес-аббревиатуры',
+          subTitle:
+            dictionary.corporate_event_calendar.generalInformation.subTitle_2,
           text: [
-            'EBITDA (Earnings Before Interest, Taxes, Depreciation, and Amortization) – финансовый показатель, который измеряет прибыль компании до учета процентов по займам, налогов, амортизации и расходов на погашение долгов.',
-            'ROA (Return on Assets) – показатель эффективности использования активов компании',
-            'ROE (Return on Equity) – показатель доходности собственного капитала компании.',
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_2_text_1,
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_2_text_2,
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_2_text_3,
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_2_text_4,
+          ],
+        },
+        {
+          subTitle:
+            dictionary.corporate_event_calendar.generalInformation.subTitle_3,
+          text: [
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_3_text_1,
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_3_text_2,
+            dictionary.corporate_event_calendar.generalInformation
+              .subTitle_3_text_3,
           ],
         },
       ],
