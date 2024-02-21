ab3679a6574673550ecd21148b491e2084da7c30
Arman Kenjebay <romawka47@gmail.com>
2024-02-21 16:07:28 +0600

    add api get-quotes

--- src/app/[locale]/(main)/investors/_components/CalculateModal.tsx
+++ src/app/[locale]/(main)/investors/_components/CalculateModal.tsx
@@ -33,7 +33,7 @@ export default function CalculateModal() {
 
   return (
     <div
-      className={`group bg-opacity-50  px-5 py-[23px] xl:flex flex-col gap-y-3 items-center justify-between rounded-xl bg-white hidden`}
+      className={`group  px-5 py-[23px] xl:flex flex-col gap-y-3 items-center justify-between rounded-xl bg-white hidden`}
     >
       <div className={`text-black`}>
         <span className={`text-[24px] font-normal`}>
@@ -41,38 +41,36 @@ export default function CalculateModal() {
         </span>
       </div>
 
-      {quotes && (
-        <div
-          className={`flex bg-white py-3 px-10 rounded-xl flex-col gap-y-3 items-center text-[21px]`}
-        >
-          <div>
-            <span className={`text-black mr-1`}>{quotes.clean_price}</span>
-            <span className={`text-red-600`}>&#8595;</span>
-            <span className={`text-red-600`}>{quotes.yield}</span>
-            <span className={`text-red-600`}>-2.68%</span>
-          </div>
-
-          <div className={`flex gap-x-1 text-[14px]`}>
-            <select
-              onChange={(change) => changeItem(change.target.value)}
-              className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
-            >
-              <option value={`US_GLD_`}>Gold</option>
-              <option value={`FPSTb2`}>Silver</option>
-            </select>
-            <select
-              className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
-            >
-              <option>USD</option>
-            </select>
-            <select
-              className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
-            >
-              <option>oz</option>
-            </select>
-          </div>
+      <div
+        className={`flex bg-white py-3 px-10 rounded-xl flex-col gap-y-3 items-center text-[21px]`}
+      >
+        <div>
+          <span className={`text-black mr-1`}>{quotes?.clean_price}</span>
+          <span className={`text-red-600`}>&#8595;</span>
+          <span className={`text-red-600`}>{quotes?.yield}</span>
+          <span className={`text-red-600`}>-2.68%</span>
         </div>
-      )}
+
+        <div className={`flex gap-x-1 text-[14px]`}>
+          <select
+            onChange={(change) => changeItem(change.target.value)}
+            className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
+          >
+            <option value={`US_GLD_`}>Gold</option>
+            <option value={`FPSTb2`}>Silver</option>
+          </select>
+          <select
+            className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
+          >
+            <option>USD</option>
+          </select>
+          <select
+            className={`w-[80px] bg-white text-gray-500 p-2 border-1 border-gray-200 rounded-lg outline-0`}
+          >
+            <option>oz</option>
+          </select>
+        </div>
+      </div>
     </div>
   )
 }
